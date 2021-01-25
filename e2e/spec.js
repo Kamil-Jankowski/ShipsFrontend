6a46cf08d6740cfc34c8831701dfe9377524391b
Hubert Milczarek <hb.milczarek@gmail.com>
2021-01-25 23:38:43 +0100

    added another changes to e2e test specs

--- e2e/spec.js
+++ e2e/spec.js
@@ -1,42 +1,40 @@
 function delay(ms) {
-  return new Promise( resolve => setTimeout(resolve, ms) );
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-describe('Ships the game', function() {
-  it('should navigate to correct sites after clicking end the game button', function() {
+describe('Ships the game', function () {
+  it('should navigate to correct sites after clicking end the game button', function () {
     browser.waitForAngularEnabled(false);
     var log4js = require("log4js");
     var logger = log4js.getLogger();
     logger.level = "debug";
-    browser.get('https://ships-the-game.herokuapp.com/');
+    browser.get('http://localhost:4200/');
     var browser2 = browser.forkNewDriverInstance(true);
     browser.findElement(by.id('name')).sendKeys('DUMMY_PLAYER_1');
     browser2.findElement(by.id('name')).sendKeys('DUMMY_PLAYER_2');
     browser.findElement(by.id('buttonAddPlayer')).click();
     browser2.findElement(by.id('buttonAddPlayer')).click();
 
-    browser.wait(protractor.ExpectedConditions.urlIs("https://ships-the-game.herokuapp.com/game/DUMMY_PLAYER_1"), 3000).then(function(){   
-     browser.findElement(by.id('endGameButton')).click();
+    browser.wait(protractor.ExpectedConditions.urlIs("http://localhost:4200/game/DUMMY_PLAYER_1"), 3000).then(function () {
+      browser.findElement(by.id('endGameButton')).click();
     });
 
-    browser.sleep(8000).then(function(){
+    browser.sleep(8000).then(function () {
       Promise.all([
-        expect(browser.getCurrentUrl()).toEqual('https://ships-the-game.herokuapp.com/landing/loose'),
-        expect(browser2.getCurrentUrl()).toEqual('https://ships-the-game.herokuapp.com/landing/win')])
-        .then(function() {
+        expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/landing/loose'),
+        expect(browser2.getCurrentUrl()).toEqual('http://localhost:4200/landing/win')])
+        .then(function () {
           logger.debug("Promise fullfilled");
           done();
-        }).catch(function() {
-          logger.debug("Promise error");
+          browser.close();
+          browser2.close();
+        }).catch(function () {
           done.fail('somehow the Url is incorrect');
-         });
+        });
     });
-
-    logger.debug("Before promise")
-
   });
 
-  // it('should navigate third player to waiting room', function(done) {
+  // it('should navigate third player to waiting room', function (done) {
   //   browser.get('https://ships-the-game.herokuapp.com/');
   //   var browser2 = browser.forkNewDriverInstance(true);
   //   var browser3 = browser.forkNewDriverInstance(true);
@@ -50,15 +48,17 @@ describe('Ships the game', function() {
   //   browser3.findElement(by.id('name')).sendKeys('DUMMY_PLAYER_3');
   //   browser3.findElement(by.id('buttonAddPlayer')).click();
 
-  //   Promise.all([
-  //   expect(browser.getCurrentUrl()).toEqual('https://ships-the-game.herokuapp.com/game/DUMMY_PLAYER_1'),
-  //   expect(browser2.getCurrentUrl()).toEqual('https://ships-the-game.herokuapp.com/game/DUMMY_PLAYER_2'),
-  //   expect(browser3.getCurrentUrl()).toEqual('https://ships-the-game.herokuapp.com/waiting-room/DUMMY_PLAYER_3')])
-  //     .then(function() {
-  //       done();
-  //     }).catch(function() {
-  //       done.fail('somehow the Url is incorrect');
-  //      });
+  //   browser.sleep(8000).then(function () {
+  //     Promise.all([
+  //       expect(browser.getCurrentUrl()).toEqual('https://ships-the-game.herokuapp.com/game/DUMMY_PLAYER_1'),
+  //       expect(browser2.getCurrentUrl()).toEqual('https://ships-the-game.herokuapp.com/game/DUMMY_PLAYER_2'),
+  //       expect(browser3.getCurrentUrl()).toEqual('https://ships-the-game.herokuapp.com/waiting-room/DUMMY_PLAYER_3')])
+  //       .then(function () {
+  //         done();
+  //       }).catch(function () {
+  //         done.fail('somehow the Url is incorrect');
+  //       });
+  //   });
   // });
 
 
--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -17,7 +17,7 @@ import { CurrentGameStatus } from './current-game-status';
   providedIn: 'root'
 })
 export class GameService {
-  private gameUrl = 'https://ships-game-service-backend.herokuapp.com/maps';
+  private gameUrl = 'http://localhost:9090/maps';
 
   /**
    * Using injection of http client
